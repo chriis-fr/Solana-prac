99c67736831887f8003e99c4855d91e146c64073
christopher odhiambo <caspianodhis@gmail.com>
2024-05-07 22:59:38 +0300

    today

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-import { Keypair } from "@solana/web3.js";
+import "dotenv/config"
+import { getKeypairFromEnvironment } from "@solana-developers/helpers"
 
-const keypair = Keypair.generate();
 
-console.log(`the public key is: `, keypair.publicKey.toBase58())
-console.log("secret key is :", keypair.secretKey)
-
+const keypair = getKeypairFromEnvironment("SECRET_KEY")
+console.log("secret key as follows: ", keypair)
